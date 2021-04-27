666fbda939c3f3c0c25d52e87e4924a676520a39
aloha-churchill <churchill.aloha@gmail.com>
2021-04-27 14:36:13 -0600

    graphs and final comments

--- pagerank.py
+++ pagerank.py
@@ -3,8 +3,15 @@ from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
 from time import time
 
-EPSILON = 100
 
+##CONSTANTS
+#epsilon for comparing pagerank vector
+EPSILON_TINY = 0.001
+#epsilon for comparing sorted vector of indices
+EPSILON_BIG = 10
+
+
+#initializes random stochastic link matrix of size N
 def init_matrix(N):
     col_list = []
     for i in range(N):
@@ -18,11 +25,13 @@ def init_matrix(N):
     T = np.column_stack(col_list)
     return T
 
+#checks for convergence by calculating the magnitude of the difference between two vectors
 def check_convergence(v_prev, v_curr):
     difference = np.subtract(v_prev, v_curr)
     magnitude = np.linalg.norm(difference)
     return magnitude
 
+#"correct" ordering solution for page rank vector with damping factor of 0.85 over 100 iterations
 def solution(T, N):
     u = np.full(N, 1)
     v = np.full(N, 1/N)
@@ -31,25 +40,31 @@ def solution(T, N):
         v = ((1-b)/N)*u + b*(np.dot(T,v))   
     return np.argsort(v)
 
+#calculates page rank vector using power iteration
 def page_rank(T, b, N):
     u = np.full(N, 1)
     v_prev = np.full(N, 1/N)
     iteration_count = 0
-
-    ref = solution(T, N)
+    #ref = solution(T, N)
     while True:
         iteration_count += 1
         v_curr = ((1-b)/N)*u + b*(np.dot(T,v_prev))
-        #if(check_convergence(ref, v_curr) < EPSILON):
-        #    return v_curr, iteration_count
-        if(check_convergence(np.argsort(v_curr),ref) < EPSILON):
-            return v_curr, iteration_count
 
-        if(iteration_count > 100):
+        #uses original check of convergence
+        if(check_convergence(v_prev, v_curr) < EPSILON_TINY):
             return v_curr, iteration_count
+        
+        #comparing to ref (ordering version)
+        #if(check_convergence(np.argsort(v_curr),ref) < EPSILON_BIG):
+        #    return v_curr, iteration_count
+
+        #put cap on algorithm so that it does not run indefinitly, use this for ordering version of convergence
+        #if(iteration_count > 100):
+        #    return v_curr, iteration_count
+        
         v_prev = v_curr
         
-
+#3D Plot for varying damping values AND varying sizes of networks
 def test_damping_vals_with_size():
     sizes = np.arange(1, 100, 10)
     damping_vals = np.arange(0,1,0.05)
@@ -72,15 +87,7 @@ def test_damping_vals_with_size():
     ax.scatter(x_axis, y_axis, time_complete)
     plt.show()
 
-    """
-    plt.title("Time complexity for PageRank with varying the damping factor")
-    plt.xlabel("Size of network")
-    plt.ylabel("Time in milliseconds")
-    plt.plot(damping_vals, time_diff)
-    print(damping_vals)
-    print(iter_array)
-    """
-
+#Plot for time complexity for varying sizes of networks
 def time_complexity():
     sizes = np.arange(1, 10000, 100)
     b = 0.85
@@ -99,9 +106,9 @@ def time_complexity():
     plt.ylabel("Time in milliseconds")
     plt.plot(sizes, time_diff)
 
-
+#graphs number of iterations based on damping factor
 def iteration_complexity():
-    n = 100
+    n = 50
     T = init_matrix(n)
     damping_vals = np.arange(0,1,0.05)
     iterations_arr = []
@@ -117,11 +124,8 @@ def iteration_complexity():
     plt.show()
 
 
-def main():
-    #T = init_matrix()
-    T = [[0,0,0.5], [0.5, 0, 0.5], [0.5, 1, 0]]
-    page_rank(T, 0.85)
+#Uncomment to run one of these functions
 
 #test_damping_vals()
 #time_complexity()
-iteration_complexity()
+#iteration_complexity()
